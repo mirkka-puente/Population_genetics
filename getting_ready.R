1084831da43aa59e245b889cbc424c5cb5a16fc5
mirkka29 <mirkka_92@alumni.ubc.ca>
2022-12-15 10:50:02 +0100

    commit 3

--- getting_ready.R
+++ getting_ready.R
@@ -1,7 +1,11 @@
-### Install packages
+### Information about the tutorial (all the tutorial)
+# https://grunwaldlab.github.io/Population_Genetics_in_R/Data_Preparation.html
+
+### Install packages -------
 #install.packages(c("poppr", "mmod", "magrittr", "treemap"), 
 #                 repos = "http://cran.rstudio.com", dependencies = TRUE)
-### Call libraries
+
+### Call libraries --------
 library("poppr")
 monpop <- read.genalex("monpop.csv")
 ### geneclone is a format of a document for population genetics
