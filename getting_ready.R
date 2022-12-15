e4a032ea2483059a9d4144dfed8197eabd7ee5d6
mirkka29 <mirkka_92@alumni.ubc.ca>
2022-12-15 09:42:16 +0100

    commit 1

---
+++ getting_ready.R
@@ -0,0 +1,5 @@
+### Install packages
+#install.packages(c("poppr", "mmod", "magrittr", "treemap"), 
+#                 repos = "http://cran.rstudio.com", dependencies = TRUE)
+### Call libraries
+library("poppr")
